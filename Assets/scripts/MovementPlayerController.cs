900deef37730b5a305b166ed9c77fcc5cda03fb4
Overtank079 <34961961+Overtank079@users.noreply.github.com>
2025-12-09 18:03:10 +0100

    Nueva clase con nuevo sistema de inputs

---
+++ Assets/scripts/MovementPlayerController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+public class MovementPlayerController : MonoBehaviour
+{
+    [SerializeField] float Speed = 5f;
+    [SerializeField] InputActionReference move;
+    [SerializeField] InputActionReference shoot;
+    [SerializeField] InputActionReference jump;
+
+    Vector2 rawMove = Vector2.zero;
+
+    private void OnEnable()
+    {
+        move.action.Enable();
+        shoot.action.Enable();
+        jump.action.Enable();
+
+        jump.action.started += OnJump;
+
+        move.action.started += OnMove;
+        move.action.performed += OnMove;
+        move.action.canceled += OnMove;
+
+        InputSystem.onAnyButtonPress.CallOnce(ctrl => Debug.Log($"Button {ctrl} was pressed on Device´{ctrl.devicename} ");
+    }
+    private void Update()
+    {
+        Vector3 moveToApply = new Vector3(rawMove.x, 0f, rawMove.y) * Speed * Time.deltaTime;
+        transform.Translate(moveToApply);
+    }
+
+    private void OnDisable()
+    {
+        move.action.started -= OnMove;
+        move.action.performed -= OnMove;
+        move.action.canceled -= OnMove;
+
+        jump.action.started -= OnJump;
+
+        move.action.Disable();
+        shoot.action.Disable();
+        jump.action.Disable();
+    }
+    bool isJumping = false;
+    void OnMove(InputAction.CallbackContext ctx)
+    {
+        rawMove = ctx.ReadValue<Vector2>();
+    }
+    private void OnJump(InputAction.CallbackContext ctx)
+    {
+        isJumping = ctx.ReadValueAsButton();
+
+    }
+}
