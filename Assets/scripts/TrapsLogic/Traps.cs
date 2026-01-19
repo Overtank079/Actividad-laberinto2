a46d9ee274943a93ff4739ce9a58a0fb277e83a3
Overtank079 <34961961+Overtank079@users.noreply.github.com>
2026-01-19 19:31:48 +0100

    Completado Actividad 2
    Trampas y Puertas

---
+++ Assets/scripts/TrapsLogic/Traps.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Traps : MonoBehaviour
+{
+    [SerializeField] Animator TrapAnimation;
+    [SerializeField] GameObject Player;
+    [SerializeField] private TextMeshProUGUI DeathCountText;
+    bool Playerinside = false;
+    int Muertes = 0;
+    bool Respawn = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DeathCountText.text = "Muertes: " + Muertes;
+        TrapAnimation.SetBool("IsReady?", true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Playerinside = true;
+        }
+        if (other.CompareTag("Player") && TrapAnimation.GetBool("IsReady?"))
+        {
+            StartCoroutine(Cooldown());
+        }
+        if (TrapAnimation.GetBool("Kill") && Playerinside == true && !Respawn)
+        {
+            Respawn = true;
+            Player.transform.position = new Vector3(-7, 1, 0);
+            Muertes++;
+            DeathCountText.text = "Muertes: " + Muertes;
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Playerinside = false;
+        }
+
+    }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(1);
+        TrapAnimation.SetTrigger("PlayerDetected");
+        TrapAnimation.SetBool("Kill", true);
+        TrapAnimation.SetBool("CD", true);
+        yield return new WaitForSeconds(1);
+        TrapAnimation.SetBool("Kill", false);
+        TrapAnimation.SetBool("IsReady?", false);
+        TrapAnimation.ResetTrigger("PlayerDetected");
+        yield return new WaitForSeconds(6);
+        Respawn = false;
+        TrapAnimation.SetBool("CD", false);
+        TrapAnimation.SetBool("IsReady?", true);
+    }
+}
